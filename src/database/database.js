7e83a609a7889cd6aa86c384b270e72ca5d33b0e
Cintra <gui7lk@gmail.com>
2022-12-16 19:28:24 -0300

    chore: adjust code for deploy on render

--- src/database/database.js
+++ src/database/database.js
@@ -3,11 +3,13 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const connectionDB = new Pool({
+    connectionString: process.env.DATABASE_URL,
     host: 'localhost',
     port: 5432,
     user: 'postgres',
     password: 'root',
-    database: 'shortly'
+    database: 'shortly',
+    ssl: true
 })
 
 export default connectionDB;
